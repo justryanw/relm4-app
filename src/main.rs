52dd743f05b4f86bf9c3150153d8b20fd7aa6ddc
Ryan <ryanjwalker2001@gmail.com>
2023-05-21 17:21:59 +0100

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use relm4::gtk;
+
+enum AppInput {
+    Increment,
+    Decrement,
+}
+
+struct AppModel {
+    counter: u8,
+}
+
+struct AppWidgets {
+    label: gtk::Label
+}
+
 fn main() {
     println!("Hello, world!");
 }
