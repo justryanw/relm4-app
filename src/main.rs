0ab6474b7496a3af4992617dda1d7b6e236a77ea
Ryan Walker <ryanjwalker2001@gmail.com>
2023-05-23 20:47:05 +0100

    factory

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,208 @@
-use relm4::{
-    gtk::{
-        self,
-        traits::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt},
-        Orientation,
-    },
-    ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent,
-};
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use relm4::factory::{DynamicIndex, FactoryComponent, FactorySender, FactoryVecDeque};
+use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 
-struct AppModel {
-    counter: u8,
+#[derive(Debug)]
+struct Counter {
+    value: u8,
 }
 
 #[derive(Debug)]
-enum AppMsg {
+enum CounterMsg {
     Increment,
     Decrement,
 }
 
-#[relm4::component]
-impl SimpleComponent for AppModel {
-    type Init = u8;
+#[derive(Debug)]
+enum CounterOutput {
+    SendFront(DynamicIndex),
+    MoveUp(DynamicIndex),
+    MoveDown(DynamicIndex),
+}
 
-    type Input = AppMsg;
-    type Output = ();
+#[relm4::factory]
+impl FactoryComponent for Counter {
+    type Init = u8;
+    type Input = CounterMsg;
+    type Output = CounterOutput;
+    type CommandOutput = ();
+    type Widgets = CounterWidgets;
+    type ParentInput = AppMsg;
+    type ParentWidget = gtk::Box;
 
     view! {
-        gtk::Window {
-            set_title: Some("Simple app"),
-            set_default_width: 300,
-            set_default_height: 100,
+        root = gtk::Box {
+            set_orientation: gtk::Orientation::Horizontal,
+            set_spacing: 10,
 
-            gtk::Box {
-                set_orientation: Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
+            #[name(label)]
+            gtk::Label {
+                #[watch]
+                set_label: &self.value.to_string(),
+                set_width_chars: 3,
+            },
 
-                gtk::Button {
-                    set_label: "Increment",
-                    connect_clicked[sender] => move |_| {
-                        sender.input(AppMsg::Increment);
-                    }
-                },
+            #[name(add_button)]
+            gtk::Button {
+                set_label: "+",
+                connect_clicked => CounterMsg::Increment,
+            },
 
-                gtk::Button::with_label("Decrement") {
-                    connect_clicked[sender] => move |_| {
-                        sender.input(AppMsg::Decrement);
-                    }
-                },
+            #[name(remove_button)]
+            gtk::Button {
+                set_label: "-",
+                connect_clicked => CounterMsg::Decrement,
+            },
 
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("Counter: {}", model.counter),
-                    set_margin_all: 5,
+            #[name(move_up_button)]
+            gtk::Button {
+                set_label: "Up",
+                connect_clicked[sender, index] => move |_| {
+                    sender.output(CounterOutput::MoveUp(index.clone()))
+                }
+            },
+
+            #[name(move_down_button)]
+            gtk::Button {
+                set_label: "Down",
+                connect_clicked[sender, index] => move |_| {
+                    sender.output(CounterOutput::MoveDown(index.clone()))
+                }
+            },
+
+            #[name(to_front_button)]
+            gtk::Button {
+                set_label: "To Start",
+                connect_clicked[sender, index] => move |_| {
+                    sender.output(CounterOutput::SendFront(index.clone()))
                 }
             }
         }
     }
 
+    fn output_to_parent_input(output: Self::Output) -> Option<AppMsg> {
+        Some(match output {
+            CounterOutput::SendFront(index) => AppMsg::SendFront(index),
+            CounterOutput::MoveUp(index) => AppMsg::MoveUp(index),
+            CounterOutput::MoveDown(index) => AppMsg::MoveDown(index),
+        })
+    }
+
+    fn init_model(value: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+        Self { value }
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: FactorySender<Self>) {
+        match msg {
+            CounterMsg::Increment => {
+                self.value = self.value.wrapping_add(1);
+            }
+            CounterMsg::Decrement => {
+                self.value = self.value.wrapping_sub(1);
+            }
+        }
+    }
+}
+
+struct App {
+    created_widgets: u8,
+    counters: FactoryVecDeque<Counter>,
+}
+
+#[derive(Debug)]
+enum AppMsg {
+    AddCounter,
+    RemoveCounter,
+    SendFront(DynamicIndex),
+    MoveUp(DynamicIndex),
+    MoveDown(DynamicIndex),
+}
+
+#[relm4::component]
+impl SimpleComponent for App {
+    type Init = u8;
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_title: Some("Factory example"),
+            set_default_size: (300, 100),
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 5,
+                set_margin_all: 5,
+
+                gtk::Button {
+                    set_label: "Add counter",
+                    connect_clicked => AppMsg::AddCounter,
+                },
+
+                gtk::Button {
+                    set_label: "Remove counter",
+                    connect_clicked => AppMsg::RemoveCounter,
+                },
+
+                #[local_ref]
+                counter_box -> gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 5,
+                }
+            }
+        }
+    }
+
+    // Initialize the UI.
     fn init(
         counter: Self::Init,
-        window: &Self::Root,
+        root: &Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = AppModel { counter };
+        let counters = FactoryVecDeque::new(gtk::Box::default(), sender.input_sender());
+        let model = App {
+            created_widgets: counter,
+            counters,
+        };
 
+        let counter_box = model.counters.widget();
         let widgets = view_output!();
 
         ComponentParts { model, widgets }
     }
 
-    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
-        match message {
-            AppMsg::Increment => {
-                self.counter = self.counter.saturating_add(1);
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            AppMsg::AddCounter => {
+                self.counters.guard().push_back(self.created_widgets);
+                self.created_widgets = self.created_widgets.wrapping_add(1);
             }
-            AppMsg::Decrement => {
-                self.counter = self.counter.saturating_sub(1);
+            AppMsg::RemoveCounter => {
+                self.counters.guard().pop_back();
+            }
+            AppMsg::SendFront(index) => {
+                self.counters.guard().move_front(index.current_index());
+            }
+            AppMsg::MoveDown(index) => {
+                let index = index.current_index();
+                let new_index = index + 1;
+                // Already at the end?
+                if new_index < self.counters.len() {
+                    self.counters.guard().move_to(index, new_index);
+                }
+            }
+            AppMsg::MoveUp(index) => {
+                let index = index.current_index();
+                // Already at the start?
+                if index != 0 {
+                    self.counters.guard().move_to(index, index - 1);
+                }
             }
         }
     }
 }
 
 fn main() {
-    let app = RelmApp::new("relm4.test.simple_maual");
-    app.run::<AppModel>(0);
+    let app = RelmApp::new("relm4.example.factory");
+    app.run::<App>(0);
 }
